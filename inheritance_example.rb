676c44d072e4b24c13371f412178cce7fdeb6b80
Lindsey Katz <katz.lindsey@gmail.com>
2019-02-19 20:52:21 -0600

    Kept initialize methods dry, added more description

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -49,7 +49,8 @@ class Vehicle
   attr_reader :speed, :direction
   attr_writer :speed, :direction
 
-  def initialize
+  def initialize(input_options_hash)
+    input_options_hash
     @speed = 0
     @direction = 'north'
   end
@@ -68,19 +69,42 @@ class Vehicle
 end
 
 class Car < Vehicle
+
+  attr_writer :fuel, :make, :model, :year
+  attr_reader :fuel, :make, :model, :year
+
+  def initialize(input_options_hash)
+    super(input_options_hash)
+    @fuel = input_options_hash[:fuel]
+    @make = input_options_hash[:make]
+    @model = input_options_hash[:model]
+    @year = input_options_hash[:year]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+
+  attr_writer :type, :weight, :color
+  attr_reader :type, :weight, :color
+
+  def initialize(input_options_hash)
+    super(input_options_hash)
+    @type = input_options_hash[:type]
+    @weight = input_options_hash[:weight]
+    @color = input_options_hash[:color]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
-bike = Bike.new
+car = Car.new(fuel: "diesel", make: "Hyundai", model: "Accent", year: 2010)
+bike = Bike.new(type: "regular", weight: 55, color: "blue")
 
 puts car.accelerate
 puts bike.accelerate
@@ -92,4 +116,6 @@ puts car.direction
 puts car.speed
 car.brake
 puts car.speed
+puts car.make
 
+# Add car specific attributes (instance variables) to the Car class (fuel, make, model, etc.) and bicycle specific attributes to the Bicycle class (type, weight, etc.). Use the super method to keep the initialize methods DRY.
